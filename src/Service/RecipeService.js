cafbfd16b345280f958f20e54d5ab312543f5dfa
shannahh <shannenshamdeo@gmail.com>
2025-01-16 11:45:30 +0100

    Seperate Diet filtering function added

--- src/Service/RecipeService.js
+++ src/Service/RecipeService.js
@@ -1,6 +1,18 @@
 const YOUR_APP_ID = 'd69c0a8f';
 const YOUR_APP_KEY = '1356aef348bcede81fae794742f196e1';
 
+//Get recipes by diet
+export const fetchRecipesByDiet = async (ingredients, diet) => {
+    const apiUrl = `https://api.edamam.com/api/recipes/v2?type=public&q=${ingredients}&diet=${diet}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (!response.ok) {
+        throw new Error(data.message || 'Error fetching recipes');
+    }
+    return data.hits || [];
+};
+
+
 export const fetchFilteredRecipes = async (ingredients, diet, allergy, cuisineType) => {
     const filterParams = [];
     if (diet) filterParams.push(`diet=${diet}`);
